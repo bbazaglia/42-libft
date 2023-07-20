60a778d698b7cca39c750e8cc49fe52635178828
Beatriz Bazaglia Sanches <bbazagli@c1r4p2.42sp.org.br>
2023-07-20 12:38:18 -0300

    atoi done

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,16 +6,39 @@
 /*   By: bbazagli <bbazagli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 11:47:02 by bbazagli          #+#    #+#             */
-/*   Updated: 2023/07/20 12:01:55 by bbazagli         ###   ########.fr       */
+/*   Updated: 2023/07/20 12:37:38 by bbazagli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//  returns the converted integer value
-// if the input string cannot be converted to a valid integer, it returns 0
+/* how atoi works?
+- skip whitespace characters;
+- check the first non-whitespace character:
+        if it's a sign (+ or -), consider it for the conversion. the next character must be a digit.
+        if it's a digit, iterate as long as the characters are digits and return the converted value.
+        if it's not a digit, return 0.
+*/
 
 #include "libft.h"
 
 int		ft_atoi(const char *nptr)
 {
-    
+    int sign;
+    int sum;
+
+    sign = 1;
+    sum = 0;
+    while (*nptr >= '\t' && *nptr <= '\r' || *nptr == ' ')
+        nptr++;
+    if (*nptr == '-' || *nptr == '-')
+    {
+        if (*nptr == '-')
+            sign *= -1;
+        nptr++;
+    }
+    while (*nptr >= '0' && *nptr <= '9')
+    {
+		sum = sum * 10 + *nptr - '0';
+		nptr++;
+	}
+	return (sum * sign);
 }
