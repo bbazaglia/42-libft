6b6e11aeb9710f958328960fd9362487e15dcd1d
Beatriz <bbazagli@student.42sp.org.br>
2023-07-25 10:05:42 -0300

    comment added

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,7 +12,18 @@
 
 #include "libft.h"
 
-// removes all characters specified in set from the start and the end of a string
+/* removes all characters specified in set from the start and the end of a string
+- first loop:
+check whether the char s1[start] exists in set
+increment the start index while the char at that position exists in set 
+once it finds a char that is not in set, the loop stops, and start will hold the index from which the trimming needs to begin
+- second loop: 
+find the ending position from which the trimming needs to end
+decrement the ebd index while the char at that position exists in set 
+once it finds a char that is not in set, the loop stops, and end will hold the index from which the trimming needs to stop
+- third loop:
+copy the trimmed characters from s1 to trimstr
+*/ 
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -28,7 +39,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 	setlen = ft_strlen(set);
 	start = 0;
 	end = s1len;
-	while (strchr(set, s1[start]) && start < s1len)
+
+	while (strchr(set, s1[start]) && start < end)
 		start++;
 	while (strchr(set, s1[end - 1]) && end > start)
 		end--;
