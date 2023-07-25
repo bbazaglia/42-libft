26cbac602a9d3ea3333759c49215fea1b03135dd
Beatriz <bbazagli@student.42sp.org.br>
2023-07-25 14:01:49 -0300

    new prototypes

--- libft.h
+++ libft.h
@@ -3,6 +3,7 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+char	**ft_split(char const *s, char c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *s, int c);
@@ -22,7 +23,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *str, int c, size_t n);
-int		ft_strlen(char *str);
+size_t	ft_strlen(const char *str);
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
