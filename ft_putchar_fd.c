41878703c6a71657d31e73d207c0b058767d9eb2
Beatriz <bbazagli@student.42sp.org.br>
2023-07-27 09:41:32 -0300

    function checked

---
+++ ft_putchar_fd.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbazagli <bbazagli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 09:37:48 by bbazagli          #+#    #+#             */
+/*   Updated: 2023/07/27 09:40:12 by bbazagli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* It outputs the character ’c’ to the given file descriptor.
+Return value: none.
+*/
+
+#include "libft.h"
+
+void ft_putchar_fd(char c, int fd)
+{
+    write(fd, &c, 1);
+}
