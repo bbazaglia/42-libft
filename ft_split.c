e0c417c6020734026dc755a64f2f8b4504e31fe2
Beatriz <bbazagli@student.42sp.org.br>
2023-07-27 15:45:56 -0300

    code commented

--- ft_split.c
+++ ft_split.c
@@ -6,28 +6,18 @@
 /*   By: bbazagli <bbazagli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 12:06:06 by bbazagli          #+#    #+#             */
-/*   Updated: 2023/07/26 12:46:56 by bbazagli         ###   ########.fr       */
+/*   Updated: 2023/07/27 15:28:51 by bbazagli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* returns the array of new strings resulting from the split (or NULL if the allocation fails)
 s: The string to be split
 c: The delimiter character
-
-- steps:
-1) count the number of occurrences of the delimiter char (how many substrings we need to allocate space for)
-2) allocate memory for the array of strings 
-*check if memory allocation was successful
-3) allocate memory for each substring
-*check if memory allocation was successful
-4) copy the content of the string to the substring
 */
 
 #include "libft.h"
-// #include <stdio.h>
 
-
-int	ft_substr_count(char const *s, char c)
+int	ft_count_words(char const *s, char c)
 {
 	int	i;
 	int	count;
@@ -36,100 +26,87 @@ int	ft_substr_count(char const *s, char c)
 	count = 0;
 	while (s[i])
 	{
+		// the first char of a word won't be a delimiter  
 		if (s[i] != c)
 		{
 			count++;
+			// iterate through the string while it's a word (as we only want to find the start of a word)
 			while (s[i] && s[i] != c)
 				i++;
 		}
+		// iterate again to find the next word
 		else 
 			i++;
 	}
+	// count equals to how many times the first letter of a word was found (number of words itself)
 	return (count);
 }
 
-char	*ft_strncpy(char *dest, const char *src, unsigned int n)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (i < n && src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-void	ft_free_split(char **arr)
+/* Deallocate memory in case there's an error during memory allocation for the words array
+When a function returns a pointer, it means that the function is providing the memory address where the data is stored rather than the actual data itself
+As the 'free' function from the C standard library expects a void * as its argument, it's natural to use a void * return type */
+void	*ft_free_split(char **arr)
 {
 	int	i;
 
 	i = 0;
 	while (arr[i])
 	{
+		/* free each word
+		in a dynamic array of strings (array of pointers), each element of the array is a pointer that points to a memory block containing a string (word)
+		these individual memory blocks are separate from the memory used to hold the array itself */
 		free(arr[i]);
 		i++;
 	}
+	/*free the array 
+	if you only free the array (but not the words first), you will have a memory leak. 
+	memory leak: the memory allocated for each word (ex: arr[0], arr[1], arr[2]) is still present in memory but cannot be accessed or freed */
 	free(arr);
+	return NULL;
+}
+
+/* allocate memory for each word and populate the words array*/
+char	**ft_write_words(char **words, char const *s, char c)
+{
+	size_t	len;
+	size_t	i;
+
+	i = 0;
+	while (*s)
+	{
+		len = 0;
+		while (s[len] != c && s[len])
+			len++;
+		words[i] = (char *)malloc(len * sizeof(char) + 1);
+		if (words[i] == NULL)
+			return (ft_free_split(words));
+		words[i][len] = '\0';
+		while (len > 0)
+		{
+			words[i][len - 1] = s[len - 1];
+			len--;
+		}
+		while (*s != c && *s)
+			s++;
+		while (*s == c && *s)
+			s++;
+		i++;
+	}
+	return (words);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**arr;
-	int		i;
-	int		j;
-	int		s_len;
-	int		substr_len;
-	int		substr_start;
-	
-	// printf("count words: %d\n", ft_substr_count(s, c));
-	arr = (char **)malloc(sizeof(char *) * (ft_substr_count(s, c) + 1));
+	size_t	num_words;
+
+	num_words = ft_count_words(s, c);
+	arr = (char **)malloc(sizeof(char *) * (num_words + 1));
 	if (arr == NULL)
 		return (NULL);
-		
-	s_len = ft_strlen(s);
-	substr_start = 0;
-	i = 0;
-	j = 0;
-	while (i < s_len)
-	{
-		if (s[i] != c)
-		{
-			substr_start = i;
-			substr_len = 0;
-			while (s[i] != c)
-			{
-				substr_len++;
-				i++;
-			}
-			arr[j] = (char *)malloc(substr_len + 1);
-			if (arr[j] == NULL)
-			{
-				ft_free_split(arr);
-				return (NULL);
-			}
-			// printf("array indice j: %d\n", j);
-			ft_strncpy(arr[j], (char *)&s[substr_start], substr_len);
-			j++;
-		}
-		else
-			i++;
-	}
-	// printf("NULL indice j: %d\n", j);
-	arr[j] = NULL;
+	while (*s == c && *s)
+		s++;
+	arr = ft_write_words(arr, s, c);
+	arr[num_words] = NULL;
 	return (arr);
 }
-
-// int main()
-// {
-// 	char **tabstr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i');
-// 	int i = 0;
-//         while (tabstr[i] != NULL)
-//         {
-//             printf("%s\n", tabstr[i]);
-//             i++;
-//         }
-
-// }
