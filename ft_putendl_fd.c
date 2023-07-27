06023cfe3a0b3ba1a2f360f707f4e80c126cfe54
Beatriz <bbazagli@student.42sp.org.br>
2023-07-27 09:58:35 -0300

    function checked

---
+++ ft_putendl_fd.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbazagli <bbazagli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 09:47:01 by bbazagli          #+#    #+#             */
+/*   Updated: 2023/07/27 09:54:05 by bbazagli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* It outputs the string ’s’ to the given file descriptor followed by a newline.
+Return value: none.
+*/
+
+#include "libft.h"
+
+void ft_putendl_fd(char *s, int fd)
+{
+    while(*s)
+    {
+        write(fd, s, 1);
+        s++;
+    }
+    write(fd, "\n", 1);
+}
---
+++ ft_putstr_fd.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbazagli <bbazagli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 09:42:27 by bbazagli          #+#    #+#             */
+/*   Updated: 2023/07/27 09:50:34 by bbazagli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* It outputs the string ’s’ to the given file descriptor.
+Return value: none.
+*/
+
+#include "libft.h"
+
+void ft_putstr_fd(char *s, int fd)
+{
+    while(*s)
+    {
+        write(fd, s, 1);
+        s++;
+    }
+}
