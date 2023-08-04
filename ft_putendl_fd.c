b2554b181c5823a7c4a5db7f43c61dee91321bfa
Beatriz <bbazagli@student.42sp.org.br>
2023-08-04 15:29:58 -0300

    check if the pointer parameter is valid

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: bbazagli <bbazagli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 09:47:01 by bbazagli          #+#    #+#             */
-/*   Updated: 2023/08/04 13:01:59 by bbazagli         ###   ########.fr       */
+/*   Updated: 2023/08/04 15:29:13 by bbazagli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ Return value: none. */
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	/* it prevents the function from accessing memory it should not access if the pointer is null
+	/* It prevents the function from attempting to dereference a null pointer, 
+	which could lead to a segmentation fault or undefined behavior (accessing memory it shouldn't access). 
+	With this check, the function will only operate on a valid string pointer
 	*/
 	if (!s)
 		return ;
