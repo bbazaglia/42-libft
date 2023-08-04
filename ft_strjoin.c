c9214f646658822c3c801d537aef63138af2555c
Beatriz <bbazagli@student.42sp.org.br>
2023-08-04 15:54:17 -0300

    note about index starting at -1 and pre-iteration

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -24,19 +24,25 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
+
 	s1len = ft_strlen(s1);
 	s2len = ft_strlen(s2);
 	newlen = s1len + s2len;
+
 	new = (char *)malloc(sizeof(char) * (newlen + 1));
 	if (new == NULL)
 		return (NULL);
+
 	i = -1;
 	while (s1[++i])
 		new[i] = s1[i];
+
 	i = -1;
 	while (s2[++i])
 		new[i + s1len] = s2[i];
+
 	new[newlen] = '\0';
+	
 	return (new);
 }
 
