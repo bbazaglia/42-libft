0f8b67b12b8917907ee98ee460f7077968d848a6
Beatriz <bbazagli@student.42sp.org.br>
2023-08-08 14:06:20 -0300

    improved comment

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: bbazagli <bbazagli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 13:04:08 by bbazagli          #+#    #+#             */
-/*   Updated: 2023/08/02 13:57:01 by bbazagli         ###   ########.fr       */
+/*   Updated: 2023/08/08 14:05:11 by bbazagli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,22 @@ Finally, the pointer to the list must be set to NULL. */
 
 #include "libft.h"
 
+// '**lst' is a pointer to a pointer to the head of the linked list
 void ft_lstclear(t_list **lst, void (*del)(void*))
 {
+    // declare a temporary pointer
     t_list *tmp;
     
+    // check if the input values are valid
     if (lst == NULL || del == NULL)
         return ;
+
+    /* iterate through the list and delete each node
+    reminder: '*lst' is a pointer to the current node */
     while (*lst != NULL)
     {
-        // save the pointer to the next node before deleting the current node
+        /* access the 'next' member of the current node and save the pointer to it
+        this is done before deleting the current node, otherwise the pointer to the next node would be lost */
         tmp = (*lst)->next;
 
         // delete the current node
@@ -32,8 +39,10 @@ void ft_lstclear(t_list **lst, void (*del)(void*))
         // free the memory of the current node
         free(*lst);
         
-        // update *lst to point to the next node, which is stored in the temp variable
+        // update '*lst' to point to the next node, which is stored in the temp variable
         *lst = tmp;
     }
+
+    // ensure that the original pointer to head of the list is set to NULL (the list is now empty)
     *lst = NULL;
 }
