ebdc5964f06a97c67568a36356f7e5b3f19538c4
Beatriz <bbazagli@student.42sp.org.br>
2023-08-08 14:54:04 -0300

    added note about typecasting and pointers arithmetics

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,14 +6,20 @@
 /*   By: bbazagli <bbazagli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 11:32:32 by bbazagli          #+#    #+#             */
-/*   Updated: 2023/07/25 14:25:23 by bbazagli         ###   ########.fr       */
+/*   Updated: 2023/08/08 14:52:27 by bbazagli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-scans the initial n bytes of the memory area pointed to by s for the first instance of c
-c and n are unsigned chars
-return a pointer to the matching byte or NULL if the character does not occur in the given memory area
+n: the number of bytes to search within the memory block pointed to by s.
+
+c: the character being searched for, provided as an integer.
+-> characters are represented as integers based on their ASCII values.
+-> by accepting c as an int, the function can be used to search for any byte value, not just characters. 
+
+s: a pointer to the memory block in which the search is performed.
+
+return value: a pointer to the matching byte or NULL if the character does not occur in the given memory area
 */
 
 #include "libft.h"
@@ -23,6 +29,8 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	unsigned char	*ptr;
 	size_t			i;
 
+	/* typecast to ensure that the pointer arithmetic operates on a byte level.
+	the ṕointer will move forward by 1 byte, not by the size of the data type it's pointing to.*/
 	ptr = (unsigned char *)s;
 	i = 0;
 	while (i < n)
@@ -33,3 +41,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	}
 	return (NULL);
 }
+
+/* Note about typecasting and why it is important for pointers arithmetics:
+If you have an int* pointer and you increment it by 1, the pointer will actually move forward by the size of an integer (usually 4 or 8 bytes).
+Similarly, if you have a char* pointer and you increment it by 1, the pointer will move forward by 1 byte, which is the size of a character. */
