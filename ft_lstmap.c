b647943b12e9576133a7555b5acb95a1d5eb196f
Beatriz <bbazagli@student.42sp.org.br>
2023-08-08 12:49:35 -0300

    version added to fit the 25 lines requirement

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: bbazagli <bbazagli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 13:10:30 by bbazagli          #+#    #+#             */
-/*   Updated: 2023/08/03 16:12:15 by bbazagli         ###   ########.fr       */
+/*   Updated: 2023/08/08 12:49:03 by bbazagli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,3 +72,28 @@ t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
     // return the pointer to the first node of the new list
     return (new_list);
 }
+
+// version added to fit the 25 lines requirement
+/* t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*new_node;
+
+	if (lst == NULL || f == NULL)
+		return (NULL);
+	new_list = NULL;
+	while (lst)
+	{
+		new_node = ft_lstnew(f(lst->content));
+		if (new_node == NULL)
+		{
+			if (new_list == NULL)
+				return (NULL);
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
+} *\
